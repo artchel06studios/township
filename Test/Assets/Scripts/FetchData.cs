6d528f2b955da0d1eb6ebe784690f454dcf3695e
artchel06studios <89133574+artchel06studios@users.noreply.github.com>
2021-08-19 11:49:57 +0800

    Update FetchData.cs

--- Test/Assets/Scripts/FetchData.cs
+++ Test/Assets/Scripts/FetchData.cs
@@ -9,14 +9,6 @@ using System;
 
 public class FetchData : MonoBehaviour
 {
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-    // jamjam
->>>>>>> parent of 06156ed (test1)
-=======
-    // jamjam22
->>>>>>> parent of 97bd889 (Update FetchData.cs)
     public TMP_Text row_name;
     public TMP_Text row_desc;
     public static HttpClient client;
